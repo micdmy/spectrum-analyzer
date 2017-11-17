2dd8f386ac580aca3e39db62572e3c388f2d0fa7
Unknown <micdmy2@gmail.com>
2017-11-17 17:21:09 +0100

    Probleam with bad adc reads.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,35 @@
 #include "stm32l4xx.h"
 #include "user.h"
 #include "DConfig.h"
+
+#define buffersize 16ul
+uint16_t buffer[16];
+void fillBufferWith(uint16_t fill) {
+	for(unsigned int i=0; i<buffersize; i++) {
+		buffer[i] = fill;
+	}
+}
 int main(void)
 {
+	mcoOutputPinEnable();
 	/*
 	 * PLL is working, source clock is MSI.
 	 * SysClk = 80MHz. MCO available on GPIOA, pin 8 (Nucleo D9 goldpin)
 	 */
-  mcoOutputPinEnable();
-  adcConfig();
-  while (1)
-  {
-  }
-  /*
-   * Before enabling ADC:
-   */
-  //ADC->CCR |= ADC_CCR_CKMODE_0; //AHB as a clock source for ADC's
-  //ADC-CCR &= ~(ADC_CCR_PRESC); //prescaller for ADC clock set to 1
 
-  //SQR - channel selectino
+	fillBufferWith(0xAA);
+
+	adcConfig();
+	dmaForAdcConfig((uint8_t *) buffer, buffersize);
+
+	dmaForAdcStart();
+	adcStart();
+
+	while(1) {
+
+	}
+
+
+
 
 }
